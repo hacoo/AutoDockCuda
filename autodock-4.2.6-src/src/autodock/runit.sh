41fdb9fc67ef92407f4dca9903f0c2a3d99dc97e
Charles Hanchett <charlie.hanchett@gmail.com>
2015-08-06 18:42:07 -0700

    made runit.sh home directory agnostic

--- autodock-4.2.6-src/src/autodock/runit.sh
+++ autodock-4.2.6-src/src/autodock/runit.sh
@@ -1,17 +1,23 @@
 #/bin/bash
 
-cd /u/hcooney/CS510GPGPU/AutoDockCuda/autodock-4.2.6-src/src/autodock/EXAMPLES/1dwd/dock_rigidlig
+
+AUTODOCK_PATH=`pwd`/autodock4
+
+cd ./EXAMPLES/1dwd/dock_rigidlig
 
 OUTPUT="output_logfile.dlg"
 INPUT="1dwd_1dwd.dpf"
-AUTODOCK_PATH="/u/hcooney/CS510GPGPU/AutoDockCuda/autodock-4.2.6-src/src/autodock/autodock4"
 
 echo "Running Autodock in directory: " $PWD
 
 echo "Input: " $INPUT
 echo "Output: " $OUTPUT
 
-echo "Starting autodock with nvidia profiler..."
+if [ "$1" = "debug" ]; then
+    echo "Starting autodock with nvidia profiler..."
 
-nvprof $AUTODOCK_PATH -p $INPUT -l $OUTPUT
-
+    nvprof $AUTODOCK_PATH -p $INPUT -l $OUTPUT
+else
+    echo "Starting autodock..."
+    $AUTODOCK_PATH -p $INPUT -l $OUTPUT
+fi
