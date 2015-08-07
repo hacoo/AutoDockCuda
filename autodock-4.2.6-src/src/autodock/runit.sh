280ac34005f1fa7dd26cd0b38f1532997cea98d4
Henry Cooney <hacoo36@gmail.com>
2015-08-06 19:15:08 -0700

    Runit now uses option nvprof for profiler, gdb for gdb

--- autodock-4.2.6-src/src/autodock/runit.sh
+++ autodock-4.2.6-src/src/autodock/runit.sh
@@ -13,10 +13,13 @@ echo "Running Autodock in directory: " $PWD
 echo "Input: " $INPUT
 echo "Output: " $OUTPUT
 
-if [ "$1" = "debug" ]; then
+if [ "$1" = "nvprof" ]; then
     echo "Starting autodock with nvidia profiler..."
 
     nvprof $AUTODOCK_PATH -p $INPUT -l $OUTPUT
+elif [ "$1" = "gdb" ]; then
+    echo "Starting autodock with GDB"
+    gdb --args $AUTODOCK_PATH -p $INPUT -l $OUTPUT
 else
     echo "Starting autodock..."
     $AUTODOCK_PATH -p $INPUT -l $OUTPUT
