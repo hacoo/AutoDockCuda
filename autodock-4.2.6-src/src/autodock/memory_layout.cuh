4148ebd6ec20dc8033a6c1b069913f5813b36550
Henry Cooney <hacoo36@gmail.com>
2015-08-06 17:35:51 -0700

    Added memory_layout.cuh

---
+++ autodock-4.2.6-src/src/autodock/memory_layout.cuh
@@ -0,0 +1,30 @@
+/* Henry Cooney <hacoo36@gmail.com> <Github: hacoo>
+   AutoDockCuda: <https://github.com/hacoo/AutoDockCuda>
+
+   memory_layout.h
+    
+   Header file for memory_layout.cu, written by Patrick Romero <Github: patrick38894>
+   
+   memory_layout.cu includes functions for initializing a population on
+   the GPU.
+
+*/
+
+
+#ifndef MEMORY_LAYOUT_H
+#define MEMORY_LAYOUT_H
+#endif
+
+#ifndef CUDA_HEADERS
+#include "/pkgs/nvidia-cuda/5.5/include/cuda.h"
+#include "/pkgs/nvidia-cuda/5.5/include/cuda_runtime.h"
+#endif
+
+
+// Function prototypes
+bool allocate_pop_to_gpu(Population & pop_in);
+__device__ Real * getIndvAttribute(int idx);
+__device__ Real * getTorsion(int indvIdx, int torsionIdx);
+__device__ char*  getAtom(int indvIdx, int atom);
+
+
